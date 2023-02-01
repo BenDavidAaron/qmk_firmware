f54c3b7fec7cd6b319214a0d02947a2bd49f8793
Ben Aaron <ben@theaarons.com>
2023-02-01 15:04:03 -0800

    format

--- keyboards/bastardkb/skeletyl/keymaps/vimyroku/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/vimyroku/keymap.c
@@ -17,7 +17,7 @@
 
 #include QMK_KEYBOARD_H
 
-    const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_split_3x5_3(
   //,-----------------------------------------------------.            ,-----------------------------------------------------.
